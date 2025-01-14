7794914c8b1baa037566e801db109122950bd61e
lilahaugust <lilahraeaugust@gmail.com>
2025-01-13 19:46:53 -1000

    styling cleanup and removal of unused code

--- src/app/constants/table.ts
+++ src/app/constants/table.ts
@@ -1,13 +1,3 @@
-export const SOLACE_ADVOCATES_TABLE_HEADERS = [
-    { id: "firstName", label: "First Name", minWidth: 150 },
-    { id: "lastName", label: "Last Name", minWidth: 150 },
-    { id: "city", label: "City", minWidth: 150 },
-    { id: "degree", label: "Degree", minWidth: 150 },
-    { id: "specialties", label: "Specialties", minWidth: 150 },
-    { id: "yearsOfExperience", label: "Years of Experience", minWidth: 150 },
-    { id: "phoneNumber", label: "Phone Number", minWidth: 150 },
-  ];
-
 export const SOLACE_ADVOCATES_FORMATTED_TABLE_HEADERS = [
   { id: "name", label: "name", minWidth: 150 },
   { id: "city", label: "city", minWidth: 150 },
